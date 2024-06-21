dc5bf3ba2220a3a51a093daf813dbe855693ad32
PramodSamota <pramod_ks@es.iitr.ac.in>
2024-06-21 22:02:07 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,9 @@ function App() {
   return (
     <>
     <div className=" white bg-red-400">
-      <h1 className=" border-blue-500 bor text-3xl font-bold underline">
+      < className=" border-blue-500 bor text-3xl font-bold underline">
       Hello world!
-    </h1>
-      <h1 className=" border-blue-500 bor text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
       <h1 className=" border-blue-500 bor text-3xl font-bold underline">
       Hello world!
     </h1>
