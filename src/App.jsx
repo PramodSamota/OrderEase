26784ae54c1b0bffefb2b9ed2e73f2ec4b4a01d9
PramodSamota <pramod_ks@es.iitr.ac.in>
2024-06-21 21:56:11 +0530

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+function App() {
+
+  return (
+    <>
+    <div className=" white bg-red-400">
+      <h1 className=" border-blue-500 bor text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <h1 className=" border-blue-500 bor text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <h1 className=" border-blue-500 bor text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <h1 className=" border-blue-500 bor text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+        
+    </>
+  )
+}
+
+export default App
